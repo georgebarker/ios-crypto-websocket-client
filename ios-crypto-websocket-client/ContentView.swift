a5b51b83b5d95ea12be638ff8018e9f2baa64d60
georgebarker <georgebarker1@me.com>
2021-08-01 13:33:10 +0200

    First commit

---
+++ ios-crypto-websocket-client/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ios-crypto-websocket-client
+//
+//  Created by George Barker on 01.08.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios-crypto-websocket-client/ios_crypto_websocket_clientApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_crypto_websocket_clientApp.swift
+//  ios-crypto-websocket-client
+//
+//  Created by George Barker on 01.08.21.
+//
+
+import SwiftUI
+
+@main
+struct ios_crypto_websocket_clientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
