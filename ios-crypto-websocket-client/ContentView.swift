b9404b0bc0e19a6c1e34a13b818ab2b5988a5f2f
georgebarker <georgebarker1@me.com>
2021-08-01 15:11:49 +0200

    Initialise the view with static data

--- ios-crypto-websocket-client/ContentView.swift
+++ ios-crypto-websocket-client/ContentView.swift
@@ -8,14 +8,67 @@
 import SwiftUI
 
 struct ContentView: View {
+    var cryptocurrencies: [Cryptocurrency] = []
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                ForEach(cryptocurrencies) { cryptocurrency in
+                    CryptocurrencyCell(cryptocurrency: cryptocurrency)
+                }
+                HStack {
+                    Spacer()
+                    Text("\(cryptocurrencies.count) Cryptocurrencies")
+                        .foregroundColor(.secondary)
+                    Spacer()
+                }
+            }.navigationTitle("Cryptocurrencies")
+        }
+
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(cryptocurrencies: testData)
+    }
+}
+
+struct CryptocurrencyCell: View {
+    var cryptocurrency: Cryptocurrency
+    var body: some View {
+        NavigationLink(destination: Text(cryptocurrency.name)) {
+            Image(cryptocurrency.thumbnailName)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 28, height: 28)
+            
+            
+            let priceText = "$\(cryptocurrency.last)"
+            VStack(alignment: .leading) {
+                Text(cryptocurrency.name)
+                Text(priceText)
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+            }
+            
+            Spacer()
+            
+            let bidText = "Bid: $\(cryptocurrency.bid)"
+            let askText = "Ask: $\(cryptocurrency.ask)"
+            VStack(alignment: .trailing) {
+                
+                Text(bidText)
+                    .font(.subheadline)
+                    .foregroundColor(Color.blue)
+                
+                
+                Text(askText)
+                    .font(.subheadline)
+                    .foregroundColor(Color.red)
+            }
+        }
+        .padding(.vertical, 4.0)
+        .padding(.horizontal, 2.0)
     }
 }
---
+++ ios-crypto-websocket-client/Cryptocurrency.swift
@@ -0,0 +1,26 @@
+//
+//  Cryptocurrency.swift
+//  ios-crypto-websocket-client
+//
+//  Created by George Barker on 01.08.21.
+//
+
+import Foundation
+
+struct Cryptocurrency: Identifiable {
+    var id = UUID()
+    var name: String
+    var bid: Decimal
+    var ask: Decimal
+    var last: Decimal
+    
+    var thumbnailName: String { return name.lowercased() + "Thumb" }
+}
+
+let testData = [
+    Cryptocurrency(name: "Bitcoin", bid: 34_560.50, ask: 34_570.00, last: 34_565.22),
+    Cryptocurrency(name: "Litecoin", bid: 250.40, ask: 255.35, last: 252.00),
+    Cryptocurrency(name: "Ethereum", bid: 2000, ask: 2200, last: 2300),
+    Cryptocurrency(name: "Ripple", bid: 0.5, ask: 0.675, last: 0.666),
+    Cryptocurrency(name: "Dogecoin", bid: 0.25, ask: 0.21, last: 0.21)
+]
