6cadef64a15bfef7e458be3c4b42708cd6e2ade7
georgebarker <georgebarker1@me.com>
2021-08-03 22:45:35 +0200

    Make a basic call to the WebSocket and print the result to the logs

--- ios-crypto-websocket-client/ios_crypto_websocket_clientApp.swift
+++ ios-crypto-websocket-client/ios_crypto_websocket_clientApp.swift
@@ -9,9 +9,43 @@ import SwiftUI
 
 @main
 struct ios_crypto_websocket_clientApp: App {
+    init() {
+        setupWebSocket()
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
+        }
+    }
+}
+
+private extension ios_crypto_websocket_clientApp {
+    func setupWebSocket() {
+        let urlSession = URLSession(configuration: .default)
+        let url = URL(string: "ws://localhost:8081/crypto-websocket-server/cryptocurrencies")
+        let webSocketTask = urlSession.webSocketTask(with: url!)
+        
+
+        webSocketTask.resume()
+        receiveOnWebSocket(with: webSocketTask)
+    }
+    
+    func receiveOnWebSocket(with webSocketTask: URLSessionWebSocketTask) {
+        webSocketTask.receive { result in
+            switch result {
+            case .failure(let error):
+                print("Failed to receive message: \(error)")
+            case .success(let message):
+                switch message {
+                case .string(let text):
+                    print("Received text message: \(text)")
+                case .data(let data):
+                    print("Received binary message: \(data)")
+                @unknown default:
+                    fatalError()
+                }
+                receiveOnWebSocket(with: webSocketTask)
+            }
         }
     }
 }
